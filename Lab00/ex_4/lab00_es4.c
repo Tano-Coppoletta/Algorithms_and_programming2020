496cb538be8fa517fbc15659b3ef5fe8a2fca3d3
Gaetano Coppoletta <tano.coppoletta@gmail.com>
2020-06-22 12:50:27 +0200

    Add files via upload

---
+++ Lab00/ex_4/lab00_es4.c
@@ -0,0 +1,46 @@
+//
+// Created by tanoc on 05/10/2019.
+//
+#include <stdio.h>
+#define maxN 20
+
+void stampa(int m[maxN][maxN],int dim,int nr,int nc);
+
+int main(){
+    int m[maxN][maxN],i,j,max;
+    int dim=1,nr,nc;
+    FILE *fp;
+
+    do{
+        printf("inserire dimensione\n");
+        scanf("%d",&dim);
+        fp=fopen("C:\\Users\\tanoc\\Desktop\\Algoritmi e programmazione\\progetto\\lab_00_es4\\testo_es2.txt","r");
+        fscanf(fp,"%d %d",&nr ,&nc);
+        for(i=0;i<nr;i++){
+            for(j=0;j<nc;j++){
+                fscanf(fp,"%d",&m[i][j]);
+            }
+        }
+        stampa(m,dim,nr,nc);
+        fclose(fp);
+    }while(dim>=0 && dim<=maxN);
+}
+
+void stampa(int m[maxN][maxN],int dim,int nr,int nc){
+    int r=-1,c=-1,i,j,z,h;
+
+    for(i=0;i<(nr-dim);i++){
+        r++;
+        for(j=0;j<(nc-dim);j++){
+            c++;
+            for(z=r;z<(dim+r);z++){
+                for(h=c;h<(dim+c);h++){
+                    printf("%d ",m[z][h]);
+                }printf("\n");
+            }
+            printf("\n");
+        }
+    }
+}
+
+
